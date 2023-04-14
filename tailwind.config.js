972c8c4b621dc8aeb7394ab53fbe75558fa2d9f8
Jorge Vargas <jorge.vargas@gmail.com>
2023-04-14 03:40:49 -0400

    Adding RATS colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rats-ecru": "#D0BE7D",
+        "rats-black": "#000009",
+        "rats-drab-dark-brown": "#524B23",
+        "rats-taupe-gray": "#7F7373",
+        "rats-black-olive": "#302B1E"
+      },
+    },
   },
   plugins: [],
 }
 
+// Colors extracted from logo with https://coolors.co/d0be7d-000009-524b23-7f7373-302b1e
